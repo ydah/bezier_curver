46e2c7f144071e0d7fd60923c5eada8543527d98
ydah <t.yudai92@gmail.com>
2025-07-29 17:48:55 +0900

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2025-07-29
+
+- Initial release
---
+++ lib/bezier_curver.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bezier_curver/version"
+
+module BezierCurver
+  class Error < StandardError; end
+  # Your code goes here...
+end
